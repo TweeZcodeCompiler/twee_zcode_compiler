fdcbb7cce1c84b9aeb1f88bc37bdb8724fba26d0
Philip Schmiel <philip.schmiel@fu-berlin.de>
2015-05-01 20:25:36 +0200

    add ZCodeConverter

---
+++ code/TweeZcodeCompiler/ZCodeConverter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by philip on 01.05.15.
+//
+
+#include "ZCodeConverter.h"
---
+++ code/TweeZcodeCompiler/ZCodeConverter.h
@@ -0,0 +1,18 @@
+//
+// Created by philip on 01.05.15.
+//
+
+#ifndef TWEEZCODECOMPILER_ZCODECONVERTER_H
+#define TWEEZCODECOMPILER_ZCODECONVERTER_H
+
+
+#include <bitset>
+
+class ZCodeConverter {
+
+    std::bitset convertStringToZSCII(string source);
+
+};
+
+
+#endif //TWEEZCODECOMPILER_ZCODECONVERTER_H
