03a24a0ed96f414e0a20d814ba0509e50da1a99c
tobias <tobys.briese@yahoo.de>
2015-05-05 19:27:52 +0200

    reinserted bit examples

---
+++ code/TweeZcodeCompiler/bithandling/examples_bit_operations.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <string>
+#include <bitset>
+
+int main(int argc, char const *argv[])
+{
+//=============================================================Construct_bit_sets=====================================================
+
+//  initialize a bitset with 8 bits, every bit is set to 0
+	std::bitset<8> defaultBitset;
+
+//  build a bitset using the hexvalues, the bitset looks like 11101111	                        
+	std::bitset<8> hexBitset (0xEf);
+
+//  build a bitset using the string, the bitset looks like 00001111	
+	std::bitset<8> stringBitset (std::string("00001111")); 
+	
+//===========================================================Print_the_bitsets========================================================
+
+	std::cout << std::endl << "Print all bitsets" << std::endl << std::endl;
+	std::cout << "Default constructed: " << defaultBitset << std::endl;
+	std::cout << "Hex constructed:     " << hexBitset << std::endl;
+	std::cout << "String constructed:  " << stringBitset << std::endl;
+	std::cout << std::endl;
+
+/*
+ *  ===========================================================Manipulate_the_default_bitsets_bits====================================
+ */
+	std::cout << "Manipulate the bits of the default constructed bitset" << std::endl << std::endl;
+
+//=============================================================Set_all_bits===========================================================
+	defaultBitset.set();
+	std::cout << "All bits were set to 1 using the set() function:               " << defaultBitset << std::endl;
+	std::cout << "set() sets all bits to 1" << std::endl << std::endl;
+
+//=============================================================Set_the_first_bit_to_0=================================================	
+/*
+ *  Note: the first bit is the rightmost bit!
+ */
+	defaultBitset.set(0,0);
+	std::cout << "The first bit was set to 0 using the set(pos, value) function: " << defaultBitset << std::endl;
+	std::cout << "Note that the first bit is the rightmost bit!" << std::endl << std::endl;
+
+//=============================================================Set_the_last_bit_to_0==================================================
+/*
+ *  Note: the last bit is the leftmost bit!
+ */
+	defaultBitset.set(defaultBitset.size()-1,0);
+	std::cout << "The last bit was set to 0 using the set(pos, value) function:  " << defaultBitset << std::endl;
+	std::cout << "Note that the last bit is the left bit!" << std::endl << std::endl;
+
+//=============================================================Set_the_last_bit_to_1==================================================
+/*
+ *  Note: the last bit is the leftmost bit!
+ */
+	defaultBitset.set(defaultBitset.size()-1);
+	std::cout << "The last bit was set to 1 using the set(pos) function:         " << defaultBitset << std::endl;
+	std::cout << "set(pos) sets the bit at pos to 1" << std::endl << std::endl;
+
+//=============================================================Flip_the_second_last_bit===============================================
+	defaultBitset.flip(defaultBitset.size()-2);
+	std::cout << "The second last bit was flipped using the flip(pos) function:  " << defaultBitset << std::endl << std::endl;
+
+//=============================================================Flip_all_bits==========================================================
+	defaultBitset.flip();
+	std::cout << "All bits were flipped using the flip() function:               " << defaultBitset << std::endl << std::endl;
+
+
+//=============================================================Reset_the_second_last_bit==============================================
+	defaultBitset.reset(defaultBitset.size()-2);
+	std::cout << "The second last bit was reset using the reset(pos) function:   " << defaultBitset << std::endl;
+	std::cout << "reset(pos) sets the bit at pos to 0" << std::endl << std::endl;
+
+//=============================================================Reset_all_bits=========================================================
+	defaultBitset.reset();
+	std::cout << "All bits were reset using the reset() function:                " << defaultBitset << std::endl;
+	std::cout << "reset() sets all bits to 0" << std::endl << std::endl;
+
+	return 0;
+}
---
+++ code/TweeZcodeCompiler/bithandling/examples_bitset_operations.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <bitset>
+
+int main(int argc, char const *argv[])
+{
+
+//  build a bitset using the string, the bitset looks like 11001111
+	std::bitset<8> stringBitsetA (std::string("11001111")); 
+	
+//  build a bitset using the string, the bitset looks like 11110011
+	std::bitset<8> stringBitsetB (std::string("11110011")); 
+
+/*
+ *  ===========================================================Manipulate_the_string_bitsets==========================================
+ */
+
+	std::cout << std::endl << "Manipulate the string constructed bitsets" << std::endl;
+	std::cout << "Note: every bitset operation must be encapsuled in () !" << std::endl;
+	std::cout << "Strings: A = " << stringBitsetA << " B = " << stringBitsetB << std::endl << std::endl;
+
+//=============================================================AND_OR_XOR=============================================================
+
+	std::cout << "Following logical operators with two operands are supported:" << std::endl << std::endl;
+	std::cout << "AND - &, AND with Assign - &= : (A&B) = " << (stringBitsetA & stringBitsetB) << std::endl;
+	std::cout << "OR - |,  OR  with Assign - |= : (A|B) = " << (stringBitsetA | stringBitsetB) << std::endl;
+	std::cout << "XOR - ^, XOR with Assign - ^= : (A^B) = " << (stringBitsetA ^ stringBitsetB) << std::endl << std::endl;
+
+//=============================================================NEG====================================================================
+	std::cout << "Use ~ to negate a complete bitset:"<< std::endl << std::endl;
+	std::cout << "Original: " << stringBitsetA  << std::endl;
+	std::cout << "Negation: " << (~stringBitsetA) << std::endl << std::endl;
+
+//=============================================================Shifting===============================================================
+	
+	std::cout << "Following shifting operators are supported:" << std::endl << std::endl;
+	std::cout << "Original: " << stringBitsetA  << std::endl;
+	std::cout << "SHL - <<, SHL with Assign - <<= : (A>>2) = " << (stringBitsetA>>2) << std::endl;
+	std::cout << "SHR - >>, SHR with Assign - >>= : (A<<2) = " << (stringBitsetA<<2) << std::endl << std::endl;
+
+
+//=============================================================Comparison=============================================================
+	std::cout << "Following compare operators are supported:" << std::endl << std::endl;
+	std::cout << "EQ  == : (A==B) = " << (stringBitsetA == stringBitsetB) << std::endl;
+	std::cout << "NEQ != : (A!=B) = " << (stringBitsetA != stringBitsetB) << std::endl << std::endl;
+
+	return 0;
+}
