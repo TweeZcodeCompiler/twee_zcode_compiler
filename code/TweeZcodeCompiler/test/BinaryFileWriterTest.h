bd4aaa48457539c5b8d16037ba17fcb3fce67383
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2015-05-07 16:35:00 +0200

    binaryWriter v2

---
+++ code/TweeZcodeCompiler/test/BinaryFileWriterTest.h
@@ -0,0 +1,18 @@
+//
+// Created by Manuel Polzhofer on 07.05.15.
+//
+
+#ifndef TWEEZCODECOMPILER_BINARYFILEWRITERTEST_H
+#define TWEEZCODECOMPILER_BINARYFILEWRITERTEST_H
+
+#include "../BinaryFileWriter.h"
+
+class BinaryFileWriterTest {
+
+public:
+    void testBinaryFileWriter();
+
+};
+
+
+#endif //TWEEZCODECOMPILER_BINARYFILEWRITERTEST_H
