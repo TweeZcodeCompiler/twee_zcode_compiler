9a0706bb9dd0d6d8d14d5f659faf7a0d21a867e5
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2015-05-18 18:02:43 +0200

    trying to build a simple compiler pipeline for hello world

--- code/TweeZcodeCompiler/FileReader.cpp
+++ code/TweeZcodeCompiler/FileReader.cpp
@@ -10,7 +10,6 @@ string FileReader::getFilePathFromArgs(int argc, char *argv[]) {
     if ( argc != 2 )
         cout <<"Enter <filename> as parameter for call\n";
     else {
-
         ifstream the_file ( argv[1] );
         if ( !the_file.is_open() )
             cout<<"Could not open file\n";
--- code/TweeZcodeCompiler/main.cpp
+++ code/TweeZcodeCompiler/main.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
-#include <bitset>
+#include <vector>
 #include "FileReader.h"
 #include "test/MainTest.h"
+#include "RoutineGenerator.h"
+#include "BinaryFileWriter.h"
+#include "ZCodeHeader.h"
 
 using namespace std;
 
+string simpleLexer(string fileContent);
+void simpleCompilerPipeline(string fileContent);
+void printHex(std::vector<std::bitset<8>> bitsetList);
+
 int main(int argc, char *argv[]) {
     MainTest test;
-    test.runAllTest();
+   // test.runAllTest();
 
     FileReader *fileReader = new FileReader();
 
@@ -15,6 +22,64 @@ int main(int argc, char *argv[]) {
 
     string fileContent = fileReader->readFile(filePath);
     cout << fileContent;
+
+    //SIMPLE COMPILER PIPLINE FOR HELLO WORLD
+    simpleCompilerPipeline(fileContent);
+
+
     //TODO: next step call Lexer with FileContent
     return 0;
+}
+//Just for Presentation
+
+void simpleCompilerPipeline(string fileContent)
+{
+    cout << "simple hello world pipeline started";
+    //get the hello world string form twee source code
+    string token = simpleLexer(fileContent);
+    token = "abcdefghijklmnopqurstuvwxyz";
+
+    std::vector<std::bitset<8>> zCode;
+
+    //generate header
+    ZCodeHeader *header = new ZCodeHeader();
+    header -> setFileLength(800, 99);
+    header -> setRoutinesOffset(100);
+    header -> setStaticStringsOffset(200);
+
+    std::vector<std::bitset<8>> *zByteHeader = header->getHeaderBits();
+    zCode.insert(zCode.end(),zByteHeader->begin(),zByteHeader->end());
+
+
+    //generate zcode for hello world string
+    RoutineGenerator routineGenerator;
+    std::vector<std::bitset<8>> zByteCodePrint = routineGenerator.printPrintRoutine(token);
+    zCode.insert(zCode.end(),zByteCodePrint.begin(),zByteCodePrint.end());
+
+
+    printHex(zByteCodePrint);
+
+    BinaryFileWriter binaryFileWriter;
+    binaryFileWriter.write("first.z3",zCode);
+
+
+}
+
+
+string simpleLexer(string fileContent)
+{
+    std::string token = fileContent.substr(fileContent.find('\n'),fileContent.length());
+    return token;
+}
+
+void printHex(std::vector<std::bitset<8>> bitsetList)
+{
+    cout << endl;
+    for(int i = 0;i<bitsetList.size();i++)
+    {
+        bitset<8> set(bitsetList.at(i));
+        cout << hex << set.to_ulong();
+
+    }
+
 }
