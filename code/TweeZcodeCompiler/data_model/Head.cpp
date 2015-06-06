07b612ff8b04384f45a8f9a84bc724ee2321c8ba
lyrkiir <lars.hochstetter@fu-berlin.de>
2015-06-06 21:17:14 +0200

    Reworked and updated the data model
    First support for formatted text added

---
+++ code/TweeZcodeCompiler/data_model/Head.cpp
@@ -0,0 +1,39 @@
+//
+// Created by lars on 05.06.15.
+//
+
+#include "include/Passage/Head.h"
+
+#include <vector>
+#include <string>
+
+Head::Head(std::string name) {
+
+    this->name = name;
+
+}
+
+Head::Head(std::string name, std::vector <std::string> tags) {
+
+    this->name = name;
+    this->tags = tags;
+
+}
+
+std::string Head::getName() {
+
+    return this->name;
+
+}
+
+std::vector <std::string> Head::getTags() {
+
+    return this->tags;
+
+}
+
+std::string Head::to_string() {
+
+    return "Head" + std::endl + "Name: " + this->name + std::endl + "Tags: " + this->tags;
+
+}
