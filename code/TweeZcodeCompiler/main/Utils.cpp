e4598160953121a38a84855361009d2b0c71033b
Philip Schmiel <philip.schmiel@fu-berlin.de>
2015-06-07 03:11:45 +0200

    added read_char, print_char instructions AND set initial PC in header AND refactored SimpleCompilerPipeline

---
+++ code/TweeZcodeCompiler/main/Utils.cpp
@@ -0,0 +1,44 @@
+//
+// Created by philip on 07.06.15.
+//
+
+#include "Utils.h"
+using std::bitset;
+using std::vector;
+
+// splits short value up to 2 bytes and pushes them into the headerBits
+void Utils::setShortVal(unsigned short val, vector<bitset<8>> &header) {
+    bitset<16> shortVal (val);
+    bitset<8> firstHalf, secondHalf;
+
+    for (size_t i = 0; i < 8; i++) {
+        secondHalf.set(i, shortVal[i]);
+    }
+
+    for (size_t i = 8; i < 16; i++) {
+        firstHalf.set(i - 8, shortVal[i]);
+    }
+
+    header.push_back(firstHalf);
+    header.push_back(secondHalf);
+}
+
+void Utils::fillWithBytes(std::vector<std::bitset<8>>& bitVector, uint8_t value, unsigned long amountOfBytes) {
+    for (size_t i = 0; i < amountOfBytes; i++) {
+        bitVector.insert(bitVector.end(), value);
+    }
+}
+
+size_t Utils::calculateNextPackageAddress(size_t currentOffset) {
+   return ((currentOffset+8) / 8) * 8;
+}
+
+void Utils::paddingToNextPackageAddress(std::vector<std::bitset<8>> &bitset,size_t offset) {
+    size_t pkgAdrr = Utils::calculateNextPackageAddress (offset+3);
+    size_t empty = pkgAdrr - bitset.size()-offset;
+    Utils::fillWithBytes(bitset, 0, (empty > 0) ? empty : 0);
+}
+
+void Utils::append(std::vector<std::bitset<8>> &head, std::vector<std::bitset<8>> &tail) {
+    head.insert(head.end(),tail.begin(),tail.end());
+}
