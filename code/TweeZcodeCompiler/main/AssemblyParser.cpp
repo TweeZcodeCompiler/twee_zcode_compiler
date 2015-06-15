cfc3d8b0989c9b6e799edee57b6391d9496f2a5e
tobias <tobys.briese@yahoo.de>
2015-06-15 12:55:56 +0200

    added using namespace std

--- code/TweeZcodeCompiler/main/AssemblyParser.cpp
+++ code/TweeZcodeCompiler/main/AssemblyParser.cpp
@@ -9,28 +9,31 @@
 #include <bitset>
 #include <cstdint>
 
+using namespace std;
 
-const std::string AssemblyParser::ROUTINE_DIRECTIVE = ".FUNCT";
-const std::string AssemblyParser::GVAR_DIRECTIVE = ".GVAR";
+const string AssemblyParser::ROUTINE_DIRECTIVE = ".FUNCT";
+const string AssemblyParser::GVAR_DIRECTIVE = ".GVAR";
 
-const std::string AssemblyParser::NEW_LINE_COMMAND = "new_line";
-const std::string AssemblyParser::PRINT_COMMAND = "print";
-const std::string AssemblyParser::JE_COMMAND = "je";
-const std::string AssemblyParser::QUIT_COMMAND = "quit";
-const std::string AssemblyParser::READ_CHAR_COMMAND = "read_char";
-const std::string AssemblyParser::CALL_COMMAND = "call";
-const std::string AssemblyParser::JUMP_COMMAND = "jump";
-const std::string AssemblyParser::RET_COMMAND = "ret";
+const string AssemblyParser::NEW_LINE_COMMAND = "new_line";
+const string AssemblyParser::PRINT_COMMAND = "print";
+const string AssemblyParser::JE_COMMAND = "je";
+const string AssemblyParser::QUIT_COMMAND = "quit";
+const string AssemblyParser::READ_CHAR_COMMAND = "read_char";
+const string AssemblyParser::CALL_COMMAND = "call";
+const string AssemblyParser::JUMP_COMMAND = "jump";
+const string AssemblyParser::RET_COMMAND = "ret";
 
 const char AssemblyParser::SPLITTER_BETWEEN_LEXEMES_IN_AN_COMMAND = ' '; // 9 is ascii for tab
 const char AssemblyParser::STRING_IDENTIFIER = '\"'; // 9 is ascii for tab
-const std::string AssemblyParser::ASSIGNMENT_OPERATOR = "->";
+const string AssemblyParser::ASSIGNMENT_OPERATOR = "->";
 
-std::string trim(const std::string& str,
-                 const std::string& whitespace = " \t")
+
+
+string trim(const string& str,
+                 const string& whitespace = " \t")
 {
     const auto strBegin = str.find_first_not_of(whitespace);
-    if (strBegin == std::string::npos)
+    if (strBegin == string::npos)
         return ""; // no content
 
     const auto strEnd = str.find_last_not_of(whitespace);
@@ -39,24 +42,24 @@ std::string trim(const std::string& str,
     return str.substr(strBegin, strRange);
 }
 
-void AssemblyParser::readAssembly(std::istream& input, std::vector <std::bitset<8>> &highMemoryZcode,
+void AssemblyParser::readAssembly(istream& input, vector <bitset<8>> &highMemoryZcode,
                                   size_t offset) {
 
-    std::cout << "Compiler: Parse Assembly File\n";
+    cout << "Compiler: Parse Assembly File\n";
 
-    for(std::string line; getline(input, line);) {
+    for(string line; getline(input, line);) {
         line = trim(line);
-        std::vector<std::string> lineComps;
+        vector<string> lineComps;
         this->split(line, SPLITTER_BETWEEN_LEXEMES_IN_AN_COMMAND, lineComps);
         if(lineComps.size()) {
-            std::string firstComp = lineComps.at(0);
+            string firstComp = lineComps.at(0);
 
             if(line.at(0) == '.') { // directive
                 if(firstComp.compare(ROUTINE_DIRECTIVE) == 0) {
-                    std::cout << "found routine" << std::endl;
+                    cout << "found routine" << endl;
 
                     if(lineComps.size() < 2) {
-                        std::cerr << "invalid routine declaration" << std::endl;
+                        cerr << "invalid routine declaration" << endl;
                         throw;
                     }
 
@@ -65,31 +68,31 @@ void AssemblyParser::readAssembly(std::istream& input, std::vector <std::bitset<
                         finishRoutine(highMemoryZcode);
                     }
 
-                    std::string routineName = lineComps.at(1);
+                    string routineName = lineComps.at(1);
 
                     currentGenerator.reset(new RoutineGenerator(routineName, 0, highMemoryZcode, offset));
                 } else if(firstComp.compare(GVAR_DIRECTIVE) == 0) {
-                    std::cout << "found gvar" << std::endl;
+                    cout << "found gvar" << endl;
 
                     this->split(line, SPLITTER_BETWEEN_LEXEMES_IN_AN_COMMAND, lineComps);
                     if(lineComps.size() < 2) {
-                        std::cerr << "empty gvar declaration" << std::endl;
+                        cerr << "empty gvar declaration" << endl;
                     }
 
-                    std::string gvar = lineComps.at(1);
+                    string gvar = lineComps.at(1);
 
                     addGlobal(gvar);
                 }
             } else { // normal instruction
                 // check for label
                 auto pos = line.find(":");
-                if(pos != std::string::npos) {
-                    std::cout << ":::::: new label: ";
-                    std::string labelName = line.substr(0, pos);
+                if(pos != string::npos) {
+                    cout << ":::::: new label: ";
+                    string labelName = line.substr(0, pos);
                     currentGenerator->newLabel(labelName);
-                    std::cout << labelName << std::endl;
+                    cout << labelName << endl;
 
-                    std::string afterLabel = line.substr(pos + 1, line.length() - 1);
+                    string afterLabel = line.substr(pos + 1, line.length() - 1);
                     executeCommand(trim(afterLabel), *currentGenerator);
                 } else {
                     executeCommand(line, *currentGenerator);
@@ -103,30 +106,30 @@ void AssemblyParser::readAssembly(std::istream& input, std::vector <std::bitset<
     }
 }
 
-void AssemblyParser::finishRoutine(std::vector <std::bitset<8>> &highMemoryZcode) {
-    std::cout << "adding routine to zcode" << std::endl;
+void AssemblyParser::finishRoutine(vector <bitset<8>> &highMemoryZcode) {
+    cout << "adding routine to zcode" << endl;
     auto routineCode = currentGenerator->getRoutine();
     Utils::append(highMemoryZcode, routineCode);
 }
 
 
-void AssemblyParser::addGlobal(std::string globalName) {
+void AssemblyParser::addGlobal(string globalName) {
     // TODO: check if maximum gvar limit is reached
     unsigned index = (unsigned) globalVariableStack.size();
-    std::cout << "adding gvar " << globalName << " at index " << std::to_string(index) << std::endl;
+    cout << "adding gvar " << globalName << " at index " << to_string(index) << endl;
     this->globalVariableStack[globalName] = index;
 }
 
-RoutineGenerator& AssemblyParser::executePRINTCommand(const std::string &printCommand, RoutineGenerator &routineGenerator) {
+RoutineGenerator& AssemblyParser::executePRINTCommand(const string &printCommand, RoutineGenerator &routineGenerator) {
 
-    std::vector <std::string> commandParts = this->split(printCommand, AssemblyParser::STRING_IDENTIFIER);
+    vector <string> commandParts = this->split(printCommand, AssemblyParser::STRING_IDENTIFIER);
     routineGenerator.printString(commandParts.at(1));
-    std::cout << commandParts.at(1) << std::endl;
+    cout << commandParts.at(1) << endl;
     return routineGenerator;
 }
 
-RoutineGenerator& AssemblyParser::executeREADCommand(const std::string &readCommand, RoutineGenerator &routineGenerator) {
-    std::vector <std::string> commandParts = this->split(readCommand, AssemblyParser::ASSIGNMENT_OPERATOR);
+RoutineGenerator& AssemblyParser::executeREADCommand(const string &readCommand, RoutineGenerator &routineGenerator) {
+    vector <string> commandParts = this->split(readCommand, AssemblyParser::ASSIGNMENT_OPERATOR);
     if(commandParts.size() < 2) {
         // TODO: decide on whether to allow no args for read_char
         return routineGenerator;
@@ -136,82 +139,82 @@ RoutineGenerator& AssemblyParser::executeREADCommand(const std::string &readComm
     return routineGenerator;
 }
 
-RoutineGenerator& AssemblyParser::executeJECommand(const std::string &jeCommand, RoutineGenerator &routineGenerator) {
+RoutineGenerator& AssemblyParser::executeJECommand(const string &jeCommand, RoutineGenerator &routineGenerator) {
 
-    std::vector <std::string> commandParts = this->split(jeCommand, '?');
-    std::string label = commandParts.at(1);
+    vector <string> commandParts = this->split(jeCommand, '?');
+    string label = commandParts.at(1);
 
     commandParts = split(jeCommand,' ');
-    std::string variableName = commandParts.at(2);
-    std::string valueString = commandParts.at(1);
-    uint16_t value =  std::stoul(valueString); // TODO: check if this fits 8 or 16 bits, then set small or large constant
-    std::cout << variableName << "," << "" << value;
+    string variableName = commandParts.at(2);
+    string valueString = commandParts.at(1);
+    uint16_t value =  stoul(valueString); // TODO: check if this fits 8 or 16 bits, then set small or large constant
+    cout << variableName << "," << "" << value;
 
     uint8_t globalZCodeAdress = getAddressForId(variableName);
 
-    std::cout << " "  << label << std::endl;
+    cout << " "  << label << endl;
     routineGenerator.jumpEquals(label, true, globalZCodeAdress, value, true, false);
 
     return routineGenerator;
 }
 
-RoutineGenerator& AssemblyParser::executeJUMPCommand(const std::string &jumpCommand, RoutineGenerator &routineGenerator) {
+RoutineGenerator& AssemblyParser::executeJUMPCommand(const string &jumpCommand, RoutineGenerator &routineGenerator) {
 
-    std::vector <std::string> commandParts = this->split(jumpCommand, '?');
-    std::string label = commandParts.at(1);
-    std::cout << label << std::endl;
+    vector <string> commandParts = this->split(jumpCommand, '?');
+    string label = commandParts.at(1);
+    cout << label << endl;
     routineGenerator.jump(label);
 
     return routineGenerator;
 }
 
-RoutineGenerator& AssemblyParser::executeCALLCommand(const std::string &callCommand, RoutineGenerator &routineGenerator) {
+RoutineGenerator& AssemblyParser::executeCALLCommand(const string &callCommand, RoutineGenerator &routineGenerator) {
 
-    std::vector <std::string> commandParts = this->split(callCommand, ' ');
-    std::string callRoutineName = commandParts.at(1);
-    std::cout << callRoutineName << std::endl;
+    vector <string> commandParts = this->split(callCommand, ' ');
+    string callRoutineName = commandParts.at(1);
+    cout << callRoutineName << endl;
 
     routineGenerator.callRoutine(callRoutineName);
     return routineGenerator;
 }
 
 
-RoutineGenerator& AssemblyParser::executeCommand(const std::string& command, RoutineGenerator &routineGenerator) {
+RoutineGenerator& AssemblyParser::executeCommand(const string& command, RoutineGenerator &routineGenerator) {
 
-    std::vector <std::string> commandParts = this->split(command,
+    vector <string> commandParts = this->split(command,
                                                          AssemblyParser::SPLITTER_BETWEEN_LEXEMES_IN_AN_COMMAND);
 
     if(!commandParts.size()) return routineGenerator;
 
-    std::string commandPart = commandParts.at(0);
+    string commandPart = commandParts.at(0);
 
     if (commandPart.compare(AssemblyParser::NEW_LINE_COMMAND) == 0) {
         routineGenerator.newLine();
-        std::cout << ":::::: new line" << std::endl;
+        cout << ":::::: new line" << endl;
     } else if (commandPart.compare(AssemblyParser::PRINT_COMMAND) == 0) {
-        std::cout << ":::::: new print ";
+        cout << ":::::: new print ";
         routineGenerator = executePRINTCommand(command, routineGenerator);
     } else if (commandPart.compare(AssemblyParser::JE_COMMAND) == 0) {
-        std::cout << ":::::: new je ";
+        cout << ":::::: new je ";
         routineGenerator = executeJECommand(command, routineGenerator);
     } else if (commandPart.compare(AssemblyParser::QUIT_COMMAND) == 0) {
         routineGenerator.quitRoutine();
-        std::cout << ":::::: new quit" << std::endl;
+        cout << ":::::: new quit" << endl;
     } else if (commandPart.compare(AssemblyParser::READ_CHAR_COMMAND) == 0) {
         routineGenerator = executeREADCommand(command, routineGenerator);
-        std::cout << ":::::: new read" << std::endl;
+        cout << ":::::: new read" << endl;
     } else if (commandPart.compare(AssemblyParser::CALL_COMMAND) == 0) {
-        std::cout << ":::::: new call ";
+        cout << ":::::: new call ";
         routineGenerator = executeCALLCommand(command, routineGenerator);
     } else if(command.compare(AssemblyParser::JUMP_COMMAND) == 0) {
-        std::cout << ":::::: new jump ";
+        cout << ":::::: new jump ";
         routineGenerator = executeJUMPCommand(command, routineGenerator);
     } else if(command.compare(AssemblyParser::RET_COMMAND) == 0) {
-        std::cout << ":::::: new return routine ";
+        cout << ":::::: new return routine ";
         routineGenerator.returnValue(0, false);
     } else {
         // TODO: handle this error more appropriately
-        std::cout << "unknown command: " << command << std::endl;
+        cout << "unknown command: " << command << endl;
         throw;
     }
 
@@ -219,8 +222,8 @@ RoutineGenerator& AssemblyParser::executeCommand(const std::string& command, Rou
 }
 
 
-bool AssemblyParser::checkIfCommandRoutineStart(const std::string &command) {
-    std::vector <std::string> commandParts = this->split(command, ' ');
+bool AssemblyParser::checkIfCommandRoutineStart(const string &command) {
+    vector <string> commandParts = this->split(command, ' ');
     for (unsigned int i = 0; i < commandParts.size(); i++) {
         if (commandParts.at(i).compare(ROUTINE_DIRECTIVE) == 0) {
             return true;
@@ -231,7 +234,7 @@ bool AssemblyParser::checkIfCommandRoutineStart(const std::string &command) {
 }
 
 
-uint8_t AssemblyParser::getAddressForId(const std::string& id) {
+uint8_t AssemblyParser::getAddressForId(const string& id) {
     // TODO: check local variables
     if(id.compare("sp") == 0) {
         return 0x0;
@@ -239,36 +242,36 @@ uint8_t AssemblyParser::getAddressForId(const std::string& id) {
     // check global variables
     try {
         return globalVariableStack.at(id);
-    } catch(std::out_of_range) {
-        std::cout << "Could not find global " << id << std::endl;
+    } catch(out_of_range) {
+        cout << "Could not find global " << id << endl;
         throw;
     }
 }
 
-std::vector <std::string> &AssemblyParser::split(const std::string &s, char delim, std::vector <std::string> &elems) {
-    std::stringstream ss(s);
-    std::string item;
-    while (std::getline(ss, item, delim)) {
+vector <string> &AssemblyParser::split(const string &s, char delim, vector <string> &elems) {
+    stringstream ss(s);
+    string item;
+    while (getline(ss, item, delim)) {
         elems.push_back(item);
     }
     return elems;
 }
 
 
-std::vector <std::string> AssemblyParser::split(const std::string &s, char delim) {
-    std::vector <std::string> elems;
+vector <string> AssemblyParser::split(const string &s, char delim) {
+    vector <string> elems;
     split(s, delim, elems);
     return elems;
 }
 
-std::vector <std::string> AssemblyParser::split(const std::string &s, const std::string &delim) {
-    std::string s_copy = s;
+vector <string> AssemblyParser::split(const string &s, const string &delim) {
+    string s_copy = s;
     size_t pos = 0;
-    std::string token;
-    std::vector<std::string> tokens;
-    while ((pos = s_copy.find(delim)) != std::string::npos) {
+    string token;
+    vector<string> tokens;
+    while ((pos = s_copy.find(delim)) != string::npos) {
         token = s_copy.substr(0, pos);
-        std::cout << "found token " << token << std::endl;
+        cout << "found token " << token << endl;
         tokens.push_back(token);
         s_copy.erase(0, pos + delim.length());
     }
